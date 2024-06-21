0c7cea66830a641e13e2aebcbe2dbcd1dfef55f9
magantus@googlemail.com <Extrem93!>
2024-06-22 01:18:18 +0200

    update

--- databasemanager/src/test/java/de/mameie/databasemanager/sql/executor/table/TableViewSqlExecutorTest.java
+++ databasemanager/src/test/java/de/mameie/databasemanager/sql/executor/table/TableViewSqlExecutorTest.java
@@ -1,22 +1,61 @@
 package de.mameie.databasemanager.sql.executor.table;
 
+import de.mameie.databasemanager.sql.server.connection.DBServerSettings;
+import de.mameie.databasemanager.sql.server.connection.H2ConnectionFactory;
+import de.mameie.databasemanager.sql.server.database.table.model.DatabaseTableView;
 import de.mameie.databasemanager.util.check.exception.ParamException;
+
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.*;
+
+import static java.util.Arrays.asList;
 
 @SpringBootTest
 public class TableViewSqlExecutorTest {
 
-    private final String SERVER_NAME="SERVER_NAME";
-    private final String DATABASE_NAME="DATABASE_NAME";
-    private final String TABLE_NAME="TABLE_NAME";
+    private final String SERVER_NAME = "SERVER_NAME";
+    private final String DATABASE_NAME = "DATABASE_NAME";
+    private final String TABLE_NAME = "users_table";
+
+    @BeforeAll
+    static void setUp() throws SQLException {
+        System.out.println("Setup!");
+        Connection connection = H2ConnectionFactory.getInstance().getConnection();
+        PreparedStatement schemaStatement = connection.prepareStatement("create table users_table (id INT,first_name VARCHAR(50),last_name VARCHAR(50),email VARCHAR(50),gender VARCHAR(50),ip_address VARCHAR(20),domain_name VARCHAR(50))");
+        schemaStatement.execute();
+        schemaStatement.close();
+        for(String query:asList(
+                "insert into users_table (id, first_name, last_name, email, gender, ip_address, domain_name) values (1, 'Beau', 'Rigler', 'brigler0@vistaprint.com', 'Male', '69.23.120.242', '360.cn')",
+                "insert into users_table (id, first_name, last_name, email, gender, ip_address, domain_name) values (2, 'Chandler', 'Chadwyck', 'cchadwyck1@paginegialle.it', 'Male', '209.170.53.69', 'scientificamerican.com')",
+                "insert into users_table (id, first_name, last_name, email, gender, ip_address, domain_name) values (3, 'Jorry', 'Thornham', 'jthornham2@sohu.com', 'Female', '168.246.45.177', 'netvibes.com')"
+        )){
+            PreparedStatement dataStatement = connection.prepareStatement(query);
+            dataStatement.execute();
+            dataStatement.close();
+        }
+        connection.close();
+    }
+
+    @AfterAll
+    static void clear() throws SQLException {
+        System.out.println("Clear!");
+        Connection connection = H2ConnectionFactory.getInstance().getConnection();
+        PreparedStatement schemaStatement = connection.prepareStatement("drop table users_table");
+        schemaStatement.execute();
+        schemaStatement.close();
+        connection.close();
+    }
 
     @Test
-    void testIfExceptionThrowByNullServerNameInConstructor(){
+    void testIfExceptionThrowByNullServerNameInConstructor() {
         Assertions.assertThrows(
                 ParamException.class,
-                ()->TableViewSqlExecutor
+                () -> TableViewSqlExecutor
                         .builder()
                         .withServerName(null)
                         .withDatabaseName(DATABASE_NAME)
@@ -27,10 +66,10 @@ public class TableViewSqlExecutorTest {
     }
 
     @Test
-    void testIfExceptionThrowByNullDatabaseNameInConstructor(){
+    void testIfExceptionThrowByNullDatabaseNameInConstructor() {
         Assertions.assertThrows(
                 ParamException.class,
-                ()->TableViewSqlExecutor
+                () -> TableViewSqlExecutor
                         .builder()
                         .withServerName(SERVER_NAME)
                         .withDatabaseName(null)
@@ -41,10 +80,10 @@ public class TableViewSqlExecutorTest {
     }
 
     @Test
-    void testIfExceptionThrowByNullTableNameInConstructor(){
+    void testIfExceptionThrowByNullTableNameInConstructor() {
         Assertions.assertThrows(
                 ParamException.class,
-                ()->TableViewSqlExecutor
+                () -> TableViewSqlExecutor
                         .builder()
                         .withServerName(SERVER_NAME)
                         .withDatabaseName(DATABASE_NAME)
@@ -54,6 +93,22 @@ public class TableViewSqlExecutorTest {
         );
     }
 
+    @Test
+    void testTableViewIsFill() throws SQLException {
 
+        TableViewSqlExecutor executor = TableViewSqlExecutor
+                .builder()
+                .withServerName(DBServerSettings.CLOUD_XXL)
+                .withDatabaseName(DATABASE_NAME)
+                .withTableName(TABLE_NAME)
+                .build();
+        executor.changeStatus("TEST");
+        DatabaseTableView view = executor.generateTableView();
+
+        Assertions.assertEquals(view.getColSize(),7);
+        Assertions.assertEquals(view.getRowSize(),3);
+        Assertions.assertEquals(view.getDatabaseTableRows().size(),3);
+        Assertions.assertEquals(view.getDatabaseTableRows().get(0).getDatabaseTableCells().size(),7);
+    }
 
 }
