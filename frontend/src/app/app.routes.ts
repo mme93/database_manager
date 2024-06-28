822ba1c8f3cf756c10d4f8a35e6960fb62186534
magantus@googlemail.com <Extrem93!>
2024-06-29 00:11:04 +0200

    update

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
+import {DashboardComponent} from "./core/page/dashboard/dashboard.component";
 
 export const routes: Routes = [
-  {
+  {path: '**', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home',component:DashboardComponent}
 
-  }
 ];
