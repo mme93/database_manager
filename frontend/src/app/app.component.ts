f83bc01c145ac18548bce0e82ee90da6042f0964
magantus@googlemail.com <Extrem93!>
2024-06-29 19:17:17 +0200

    update

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,17 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ToolbarModule} from "primeng/toolbar";
 import {ButtonModule} from "primeng/button";
-import {SidebarModule} from "primeng/sidebar";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ToolbarModule,ButtonModule,SidebarModule],
+  imports: [RouterOutlet,ToolbarModule,ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Database Manager';
-  gfg=false;
 }
