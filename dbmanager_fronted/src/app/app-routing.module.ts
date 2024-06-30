0d806e2fe7bd3b1b9fc501bbb1f2041beb1e1a65
magantus@googlemail.com <Extrem93!>
2024-06-30 18:51:31 +0200

    update

--- dbmanager_fronted/src/app/app-routing.module.ts
+++ dbmanager_fronted/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "./core/page/dashboard/dashboard.component";
 import {LoginComponent} from "./core/page/account/login/login.component";
+import {authGuard} from "./shared/guard/auth.guard";
+
 
 const routes: Routes = [
-  {path: 'home', component: DashboardComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'dashboard', component: DashboardComponent,canActivate:[authGuard]},
+  {path: 'login', component: LoginComponent},
+  {path: '**', redirectTo: 'dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
