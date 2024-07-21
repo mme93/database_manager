73dcf8b0f9b1b536c6358d69a87ca5416eb438f9
magantus@googlemail.com <Extrem93!>
2024-07-21 04:49:57 +0200

    update

--- dbmanager_fronted/src/app/core/page/dashboard/table/show-table/show-table.component.ts
+++ dbmanager_fronted/src/app/core/page/dashboard/table/show-table/show-table.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {TableService} from "../../../../../shared/service/http/table/table.service";
 import {ActivatedRoute} from "@angular/router";
-import {DatabaseTableRow, DatabaseTableView} from "../../../../../shared/model/table/TableView";
+import { DatabaseTableView} from "../../../../../shared/model/table/TableView";
 
 @Component({
   selector: 'app-show-table',
@@ -10,12 +10,8 @@ import {DatabaseTableRow, DatabaseTableView} from "../../../../../shared/model/t
 })
 export class ShowTableComponent implements OnInit {
   databaseTableView: DatabaseTableView = {};
-  copyDatabaseTableView: DatabaseTableView = {};
   tableName: string | null = '';
   databaseName: string | null = '';
-  page: number = 0;
-  rows: number = 5;
-  pagedRows: DatabaseTableRow[] = [];
 
   constructor(private tableService: TableService, private route: ActivatedRoute) {
   }
@@ -28,29 +24,11 @@ export class ShowTableComponent implements OnInit {
       if (server) {
         this.tableService.getTableByNameAndDatabase(server, this.databaseName, this.tableName)
           .subscribe(result => {
-            this.databaseTableView = result || {metaData: [], databaseTableRows: []};
-            this.copyDatabaseTableView = result || {metaData: [], databaseTableRows: []};
-            this.updatePage();
+            this.databaseTableView = result || { metaData: [], databaseTableRows: [] };
           }, error => {
-            this.databaseTableView = {metaData: [], databaseTableRows: []};
-            this.updatePage();
+            this.databaseTableView = { metaData: [], databaseTableRows: [] };
           });
       }
     });
   }
-
-  change(event: any): void {
-    this.page = event.page;
-    this.rows = event.rows;
-    this.updatePage();
-  }
-
-  updatePage(): void {
-    const start = this.page * this.rows;
-    const end = start + this.rows;
-    if (this.copyDatabaseTableView.databaseTableRows) {
-      this.databaseTableView.databaseTableRows = this.copyDatabaseTableView.databaseTableRows.slice(start, end);
-    }
-  }
-
 }
